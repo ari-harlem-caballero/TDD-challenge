ec4b0bb3d6177745bf04a5aec71521eb8c956c7f
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-11-30 11:14:37 -0800

    tested addExclamationPoints function three times, returned correct

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,16 +1,35 @@
 // IMPORT MODULES under test here:
-import { 
-    myFunction,
+
+import {
+    addExclamationPoints, multiplyBySeven,
 } from '../functions.js';
 
 const { test, skip } = QUnit;
 
-test('this test should pass', (expect) => {
-    const expected = true;
+test('addExlamationPoints should return a string with 3 exclamation points added to the end', (expect) => {
+    const expected1 = 'frogs!!!';
+    
+    const actual1 = addExclamationPoints('frogs');
+    
+    const expected2 = 'dogs!!!';
 
-    const actual = myFunction();
+    const actual2 = addExclamationPoints('dogs');
+    
+    const expected3 = 'cows!!!'
 
-    expect.equal(actual, expected);
+    const actual3 = addExclamationPoints('cows');
+    
+    expect.equal(actual1, expected1, 'should return frogs!!!');
+    expect.equal(actual2, expected2, 'should return dogs!!!');
+    expect.equal(actual3, expected3, 'should return cows!!!');
+});
+
+test('This function should take in a number and multiply it by seven', (expect) => {
+    const expected1 = 21;
+
+    const actual1 = multiplyBySeven(3);
+
+    expect.equal(actual1, expected1, 'should return 21');
 });
 
 
